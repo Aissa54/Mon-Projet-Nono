a6905a77a87385931432124a4f27b789786d1f29
Anas <anasbv89@gmail.com>
2021-12-09 12:06:38 +0100

    Update test.js
    je modifie encore

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
 // Jordan - Je teste une modification 
-<<<<<<< HEAD
 // Jordan - Je teste une modification 2
 
 // A partir de maintenant, je suis dans la branche main
-=======
-alert("helllllllloooooo");
-const x = 50; -7
->>>>>>> f4781bb6cbc3d281af714f997cbf52b022b80324
