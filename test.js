145e2b3002c123944935748f453b08b5ef22c382
Jordan <namor.jordan@gmail.com>
2021-12-09 12:02:04 +0100

    Ajout d'une lign

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 // Jordan - Je teste une modification 
 // Jordan - Je teste une modification 2
 
-
-// A partir de maintenant, je suis dans la branche jordan
+// A partir de maintenant, je suis dans la branche main
